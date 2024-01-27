2f31da802736d446444f0ae46d799d17c93be8ac
Rohan Negi <r.negi191998@gmail.com>
2024-01-27 12:28:55 -0400

    Refactored code using Factory and Strategy design patterns

---
+++ battery/spindler_battery.py
@@ -0,0 +1,14 @@
+from utils import add_years_to_date
+from battery.battery import Battery
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service():
+        date_to_service_battery = add_years_to_date(self.last_service_date, 2)
+        if date_to_service_battery < self.current_date:
+            return True
+        else:
+            return False
